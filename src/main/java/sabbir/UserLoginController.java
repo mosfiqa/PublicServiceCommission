976981664e0b34fb76d468a4023f618492084ad4
Sabbir Ahammed Khan <2331423@iub.edu.bd>
2025-11-26 21:51:09 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/sabbir/MarketingOfficerDashboardController.java
    #	src/main/java/sabbir/PublishCircularController.java
    #	src/main/java/sabbir/SocialMediaController.java

---
+++ src/main/java/sabbir/UserLoginController.java
@@ -0,0 +1,18 @@
+package sabbir;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UserLoginController {
+    @javafx.fxml.FXML
+    private TextField userIDTF;
+    @javafx.fxml.FXML
+    private Label notificationLabel;
+    @javafx.fxml.FXML
+    private TextField passwordTF;
+
+    @javafx.fxml.FXML
+    public void LoginButtonOnAction(ActionEvent actionEvent) {
+    }
+}
