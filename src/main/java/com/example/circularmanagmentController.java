ecba9a624a579b5ff1067e9f6d514dabef148aca
Mosfiqa Akter Sohely <2331557@iub.edu.bd>
2025-11-26 12:13:23 +0600

    Initial commit

---
+++ src/main/java/com/example/circularmanagmentController.java
@@ -0,0 +1,49 @@
+package com.example;
+
+public class circularmanagmentController
+{
+    @javafx.fxml.FXML
+    private TableColumn QualificationTCOL;
+    @javafx.fxml.FXML
+    private Text De;
+    @javafx.fxml.FXML
+    private TextField nameTF;
+    @javafx.fxml.FXML
+    private TableColumn jobTCOL;
+    @javafx.fxml.FXML
+    private TextField nameTF11;
+    @javafx.fxml.FXML
+    private Text DescriptionTA;
+    @javafx.fxml.FXML
+    private Button AdminTableView;
+    @javafx.fxml.FXML
+    private Text DeadlineDatePicker;
+    @javafx.fxml.FXML
+    private Text DepartmentTF;
+    @javafx.fxml.FXML
+    private TextField nameTF1;
+    @javafx.fxml.FXML
+    private Text VacancyTF;
+    @javafx.fxml.FXML
+    private TableColumn DeadlineTCOL;
+    @javafx.fxml.FXML
+    private TableColumn DepartmentTCOL;
+    @javafx.fxml.FXML
+    private TableColumn VacancyTCOL;
+    @javafx.fxml.FXML
+    private Text QualificationTA;
+    @javafx.fxml.FXML
+    private TableColumn DescriptionTCOL;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void addButton(Event event) {
+    }
+}
